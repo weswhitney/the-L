c31ddceefcd5415bc230ab6206e96c435b8fda9c
weswhitney <wesleywhitney@gmail.com>
2017-04-01 16:08:54 -0600

    Other Bonus Handle decimals

--- calculator.js
+++ calculator.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res) {
 router.get('/add/:num1/:num2', function(req, res) {
 		var param1 = req.params.num1;
 		var param2 = req.params.num2;
-		var number1 = parseInt(param1, 10);
-		var number2 = parseInt(param2, 10);
+		var number1 = parseFloat(param1);
+		var number2 = parseFloat(param2);
 		var result = number1 + number2;
 		res.send(result.toString());
 });
@@ -17,8 +17,8 @@ router.get('/add/:num1/:num2', function(req, res) {
 router.get('/sub/:num1/:num2', function(req, res) {
 	var param1 = req.params.num1;
 	var param2 = req.params.num2;
-	var number1 = parseInt(param1);
-	var number2 = parseInt(param2);
+	var number1 = parseFloat(param1);
+	var number2 = parseFloat(param2);
 	var result = number1 - number2;
 	res.send(result.toString());
 });
@@ -26,8 +26,8 @@ router.get('/sub/:num1/:num2', function(req, res) {
 router.get('/div/:num1/:num2', function(req, res) {
 	var param1 = req.params.num1;
 	var param2 = req.params.num2;
-	var number1 = parseInt(param1);
-	var number2 = parseInt(param2);
+	var number1 = parseFloat(param1);
+	var number2 = parseFloat(param2);
 	var result = number1 / number2;
 	res.send(result.toString());
 });
@@ -35,8 +35,8 @@ router.get('/div/:num1/:num2', function(req, res) {
 router.get('/mult/:num1/:num2', function (req, res) {
 	var param1 = req.params.num1;
 	var param2 = req.params.num2;
-	var number1 = parseInt(param1);
-	var number2 = parseInt(param2);
+	var number1 = parseFloat(param1);
+	var number2 = parseFloat(param2);
 	var result = number1 * number2;
 	res.send(result.toString());
 });
