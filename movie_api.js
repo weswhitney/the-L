44e1680691c875ff709f7117b270163c259e4dda
weswhitney <wesleywhitney@gmail.com>
2017-03-31 21:00:54 -0600

    added 2nd response to array

--- movie_api.js
+++ movie_api.js
@@ -4,10 +4,10 @@ var responseArray = [];
 
 http.get('http://omdbapi.com/?i=tt0241527', function(res) {
 	responseArray.push(res);
-	console.log('527 : ' + responseArray.length + ' ' + responseArray);
-});
-
-http.get('http://omdbapi.com/?i=tt0295297', function(res) {
-	responseArray.push(res);
-	console.log('297 :' + responseArray.length);
+	res.on('data', function() {
+		http.get('http://omdbapi.com/?i=tt0295297', function(res) {
+			responseArray.push(res);
+			console.log(responseArray);
+		});
+	});
 });
